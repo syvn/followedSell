5b4942ac3a4b91daece5c8b87a9e7d1b80e60e9e
yxpselent <1007129227@qq.com>
2017-11-10 22:41:34 +0800

    添加项目

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <v-demo></v-demo>
+  </div>
+</template>
+
+<script>
+import demo from './components/demo'
+export default {
+  name: 'app',
+  components: {
+    'v-demo': demo
+  }
+}
+</script>
+  
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
